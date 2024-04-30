489dce35562beb038bd8ca27bc93fa2adfeb042c
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-04-30 07:48:49 +0300

    Finish local storage functionality

--- app.js
+++ app.js
@@ -5,9 +5,24 @@ function compile() {
     let js = document.getElementById("js");
     let code = document.getElementById("code").contentWindow.document;
     
-    document.body.onkeyup = function() {
+    document.body.onkeyup = function () {
         code.open();
         code.writeln(html.value + "<style>" + css.value + "</style>" + "<script>" + js.value + "</script>");
+        code.close();
+        // local storage functionality
+        localStorage.setItem("htmlCode",html.value);
+        localStorage.setItem("cssCode",css.value);
+        localStorage.setItem("jsCode",js.value);
+        localStorage.setItem("iframeContent",code.documentElement.innerHTML);
+    }
+    // load from local storage on page load
+    window.onload = function() {
+        html.value = localStorage.getItem("htmlCode" || '');
+        css.value = localStorage.getItem("cssCode" || '');
+        js.value = localStorage.getItem("jsCode" || '');
+        //getting iframe content
+        code.open();
+        code.writeln(localStorage.getItem("iframeContent")  || '');
         code.close();
     }
 
